2d2dd1eda61552d285297998355780c5ce48af03
muharromi42 <muharromi42@gmail.com>
2024-04-07 17:09:21 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+    public function index($nama = 'romi', $pekerjaan = 'mahasiswa')
+    {
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
