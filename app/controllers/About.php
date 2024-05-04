1c679880c76a00c440af22abb942a7331aea1c64
muharromi42 <muharromi42@gmail.com>
2024-05-04 22:40:35 +0700

    view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,23 @@
 <?php
 
-class About
+class About extends Controller
 {
-    public function index($nama = 'romi', $pekerjaan = 'mahasiswa')
+    public function index($nama = 'romi', $pekerjaan = 'mahasiswa', $umur = 20)
     {
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
