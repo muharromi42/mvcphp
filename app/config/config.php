af70366ac3b2fb696dfd08149e5b03e6f82dae32
muharromi42 <muharromi42@gmail.com>
2024-05-14 17:20:28 +0700

    database wrapper

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASEURL', 'http://localhost/mvcphp/public');
+
+
+//db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvcphp');
