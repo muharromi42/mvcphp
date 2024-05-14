2fbb1016b194c4280944244e86b4820736ea1ce1
muharromi42 <muharromi42@gmail.com>
2024-05-14 16:16:41 +0700

    models

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="mt-4 p-5 bg-primary text-white rounded">
         <h1>Selamat Datang Di website saya</h1>
-        <p>Lorem ipsum...</p>
+        <p>Halo nama saya <?= $data['nama']; ?></p>
     </div>
 </div>
